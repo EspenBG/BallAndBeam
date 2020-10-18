a1a7c35cc92f9fd3554140b3ba0e7a73c363008d
Espen <EspenBG>
2020-10-18 20:48:18 +0200

    Added logic for finding the position of the ball and sending the input to the PID controller. Also added writing of the setpoint of the servo motor

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,38 @@
 #include <PID_v1.h>
 #include <Servo.h>
 
+// Constants
 int y_limit[] = {0, 200};
-int x_limit[] = {0, 340};
+int x_limit[] = {0, 340}; // Acts as the linear approximation for the ball
+int servoPin = 9;
+double setpoint_PID = 30; // set to a value between 0 and 100
+double input_PID;
+double output_PID;
+
+
+double K_p = 1;
+double K_i = 0;
+double K_d = 0;
 
 // This is the main Pixy object
 Pixy pixy;
 
 // This is the servo object
-Servo servo;
+Servo myServo;
+
+// This is the PID object
+PID pid_controller(&input_PID, &output_PID, &setpoint_PID, K_p, K_i, K_d, DIRECT);
 
 void setup()
 {
     Serial.begin(9600);
     Serial.print("Starting...\n");
 
+
     pixy.init();
+    myServo.attach(servoPin);
+    pid_controller.SetMode(AUTOMATIC); // Turns the PID controller on
+    pid_controller.SetSampleTime(100);
 }
 
 void loop()
@@ -26,36 +43,36 @@ void loop()
     static int i = 0;
     int j;
     uint16_t blocks;
-    char buf[32];
-
     // grab blocks!
     blocks = pixy.getBlocks();
 
-    // If there are detect blocks, print them!
+    // if there are any blocks
     if (blocks)
     {
         i++;
 
-        // do this (print) every 50 frames because printing every
-        // frame would bog down the Arduino
-        if (i%50==0)
-        {
+
+
             int largestObject = 0;
             int areaLargestObject = 0;
-
-            sprintf(buf, "Detected %d:\n", blocks);
-            Serial.print(buf);
+//
+//            sprintf(buf, "Detected %d:\n", blocks);
+//            Serial.print(buf);
             for (j=0; j<blocks; j++)
             {
-                // Check and find the largest object of signature 1
-                if (y_limit[0] < pixy.blocks[j].y < y_limit[1] && x_limit[0] < pixy.blocks[j].x) {
+                // Checks if the colour of the object is correct and if the position is in the correct area
+                bool checkObjectPosition = (pixy.blocks[j].signature == 1) &&
+                        (y_limit[0] < pixy.blocks[j].y) &&
+                        (pixy.blocks[j].y < y_limit[1]) &&
+                        (x_limit[0] < pixy.blocks[j].x) &&
+                        (pixy.blocks[j].x < x_limit[1]);
+
+                if (checkObjectPosition) {
                     int areaObject = pixy.blocks[j].height * pixy.blocks[j].width;
-                    Serial.println(areaObject);
                     if (areaLargestObject >= areaObject) {
                         areaLargestObject = areaObject;
                         largestObject = j;
                     }
-
 
                 }
                 // Check for the position of the object
@@ -66,11 +83,15 @@ void loop()
 //                sprintf(buf, "  block %d: ", j);
 //                Serial.print(buf);
 //                pixy.blocks[j].print();
-                pixy.blocks[largestObject].print();
-
+                if (i = 30) {pixy.blocks[largestObject].print();}
             }
+            long inputValue = map(pixy.blocks[largestObject].x, x_limit[0], x_limit[1], 0, 100);
+            input_PID = (double) inputValue;
+            pid_controller.Compute();
+            long servoSetpoint = map(output_PID, 0, 100, 10, 170);
+            myServo.write(servoSetpoint);
 
-        }
+
     }
 
 }
